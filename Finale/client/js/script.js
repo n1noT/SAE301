8162f0e02b287afcb5e19bd7e87e4a4fdebe2bbc
Nino Toutain <nino.toutain@etu.unilim.fr>
2023-10-25 14:51:24 +0200

    it-3 14h51

--- Finale/client/js/script.js
+++ Finale/client/js/script.js
@@ -13,12 +13,26 @@ import { productRenderer } from "./renderer/product-renderer.js";
     document.querySelector("#menu").innerHTML = productRenderer(data);
  }
 
+ V.init = function(){
+    let filtres = document.querySelector('#filters');
+    filtres.addEventListener('click', C.handler_clickOnFilter);
+ }
+
  let C = {}
 
  C.init = async function(){
+    V.init();
      let nb = await M.products.load("https://mmi.unilim.fr/~toutain4/api/products");
      console.log(nb + " products added in the ProductCollection");
      V.render( M.products.findAll() );
+    
+ }
+
+ C.handler_clickOnFilter = function(ev){
+    if ( ev.target.dataset.filter != undefined )
+    {
+        V.render( M.products.findByCategory(ev.target.dataset.filter)); 
+    }
  }
 
 C.init();
